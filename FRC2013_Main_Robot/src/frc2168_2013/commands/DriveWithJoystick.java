bb901d5c4b1ca55c80f47b8509fe19e0802a8e7b
SultanJilani621616 <SultanJilani621616@gmail.com>
2013-01-18 20:39:02 -0500

    Forgot to remove @override shit. Fixed that. It builds now. :)

--- FRC2013_Main_Robot/src/frc2168_2013/commands/DriveWithJoystick.java
+++ FRC2013_Main_Robot/src/frc2168_2013/commands/DriveWithJoystick.java
@@ -3,7 +3,6 @@ package frc2168_2013.commands;
 import frc2168_2013.RobotMap;
 
 
-
 public class DriveWithJoystick extends CommandBase {
 	
 	public DriveWithJoystick(){
@@ -12,32 +11,27 @@ public class DriveWithJoystick extends CommandBase {
 		
 	}
 
-	@Override
 	protected void end() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	protected void execute() {
 		
 		driveTrain.setSpeed(oi.baseDriver.getRawAxis(RobotMap.rightJoyAxis), oi.baseDriver.getRawAxis(RobotMap.leftJoyAxis));
 
 	}
 
-	@Override
 	protected void initialize() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	protected void interrupted() {
 		// TODO Auto-generated method stub
 
 	}
 
-	@Override
 	protected boolean isFinished() {
 		
 		return false;
