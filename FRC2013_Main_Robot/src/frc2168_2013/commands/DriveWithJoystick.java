7f60795f16751cb2ef973cf6332319d854619066
James <james@team2168.org>
2013-01-20 13:12:33 -0500

    Style changes. Modified to use setPWM to drive w/ stick. setSpeed will command a speed in ft/s to be consistent w/ the getSpeed methods.

--- FRC2013_Main_Robot/src/frc2168_2013/commands/DriveWithJoystick.java
+++ FRC2013_Main_Robot/src/frc2168_2013/commands/DriveWithJoystick.java
@@ -6,36 +6,27 @@ import frc2168_2013.RobotMap;
 public class DriveWithJoystick extends CommandBase {
 	
 	public DriveWithJoystick(){
-		
 		requires(driveTrain);
-		
 	}
 
 	protected void end() {
 		// TODO Auto-generated method stub
-
 	}
 
 	protected void execute() {
-		
-		driveTrain.setSpeed(oi.baseDriver.getRawAxis(RobotMap.rightJoyAxis), oi.baseDriver.getRawAxis(RobotMap.leftJoyAxis));
-
+		driveTrain.setPWM(oi.baseDriver.getRawAxis(RobotMap.rightJoyAxis), oi.baseDriver.getRawAxis(RobotMap.leftJoyAxis));
 	}
 
 	protected void initialize() {
 		// TODO Auto-generated method stub
-
 	}
 
 	protected void interrupted() {
 		// TODO Auto-generated method stub
-
 	}
 
 	protected boolean isFinished() {
-		
 		return false;
-		
 	}
 
 }
