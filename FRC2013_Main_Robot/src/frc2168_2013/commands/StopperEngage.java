492f23465d6c8d22fb1cbb33609a11430b450d60
James <james@team2168.org>
2013-02-07 18:55:06 -0500

    End() shouldn't call itself. Interruption of a stopper engage command shouldn't disengage the   stopper. Plus this command should be impossible to interrupt, it   immediately completes. Style cleanups.

--- FRC2013_Main_Robot/src/frc2168_2013/commands/StopperEngage.java
+++ FRC2013_Main_Robot/src/frc2168_2013/commands/StopperEngage.java
@@ -7,35 +7,45 @@ package frc2168_2013.commands;
  */
 public class StopperEngage extends CommandBase {
 
+	/**
+	 * Default constructor.
+	 */
     public StopperEngage() {
     	requires(hopper);
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	
     }
 
-    // Called just before this Command runs the first time
+    /**
+     * Called just before this Command runs the first time.
+     */
     protected void initialize() {
     }
 
-    // Called repeatedly when this Command is scheduled to run
+    /**
+     * Called repeatedly when this Command is scheduled to run.
+     */
     protected void execute() {
     	hopper.engageStopper();
     }
 
-    // Make this return true when this Command no longer needs to run execute()
+    /**
+     * Returns true immediately after the stopper has been engaged.
+     */
     protected boolean isFinished() {
         return true;
     }
 
-    // Called once after isFinished returns true
+    /**
+     * Called once after isFinished returns true.
+     */
     protected void end() {
-    	end();
+    	//Nothing special to do here.
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
+    /**
+     * Called when another command which requires one or more of the same
+     * subsystems is scheduled to run.
+     */
     protected void interrupted() {
-    	hopper.disengageStopper(); //disengaged so frisbee can be shot, much better to let the frisbee go than to have it jammed in the shooter!
+    	//Nothing special to do here.
     }
 }
