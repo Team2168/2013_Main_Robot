0bd7ed0e137ac3de575986cafd58d364d4e42669
James <james@team2168.org>
2013-02-07 18:56:52 -0500

    Style fixes.

--- FRC2013_Main_Robot/src/frc2168_2013/commands/StopShooterWheel.java
+++ FRC2013_Main_Robot/src/frc2168_2013/commands/StopShooterWheel.java
@@ -6,36 +6,46 @@ package frc2168_2013.commands;
  * @author Shriji
  */
 public class StopShooterWheel extends CommandBase {
-	boolean shit; //just had to make a bollean that would not be used so i dont have to add timeout to commandgroup
-
-    public StopShooterWheel() {
+    /**
+     * Default constructor.
+     */
+	public StopShooterWheel() {
     	requires(shooter);
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    	
     }
 
-    // Called just before this Command runs the first time
+    /**
+     * Called just before this Command runs the first time.
+     */
     protected void initialize() {
+    	//Nothing special to do on initialization.
     }
 
-    // Called repeatedly when this Command is scheduled to run
+    /**
+     * Called repeatedly when this Command is scheduled to run.
+     */
     protected void execute() {
-    	shooter.setPWM(0);
+    	shooter.setPWM(0.0);
     }
 
-    // Make this return true when this Command no longer needs to run execute()
+    /**
+     * Returns true after the wheel has been stopped.
+     */
     protected boolean isFinished() {
-        return shit;
+        return true;
     }
 
-    // Called once after isFinished returns true
+    /**
+     * Called once after isFinished returns true
+     */
     protected void end() {
+    	//The wheel is stopped, nothing special needs to be done on command completion.
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
+    /**
+     * Called when another command which requires one or more of the same
+     * subsystems is scheduled to run.
+     */
     protected void interrupted() {
-    	shooter.setPWM(0);
+    	//The wheel is stopped, nothing special to do if interrupted.
     }
 }
