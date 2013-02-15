38d7cbb4deffba55c09044eb98e92bd34c6d5e50
Shriji <shriji.amin1995@gmail.com>
2013-02-15 02:46:05 -0500

    fixed issue #47

--- FRC2013_Main_Robot/src/frc2168_2013/commands/DrivePIDPosition.java
+++ FRC2013_Main_Robot/src/frc2168_2013/commands/DrivePIDPosition.java
@@ -40,7 +40,7 @@ public class DrivePIDPosition extends CommandBase {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-    	return drivetrain.leftPosController.isEnabled() == true;
+    	return drivetrain.leftPosController.isEnabled() == false;
     }
 
     // Called once after isFinished returns true
