f384fe522eb1008d6593569b68253d10aeb1be4a
Kevin Harrilal <kevinharrilal@msn.com>
2013-02-20 05:30:18 -0500

    Removed classpath from index.
    hopefully this prevents the file from being tracked.
    
    Also This commit fixes #22

--- FRC2013_Main_Robot/src/frc2168_2013/CommandBaseRobot.java
+++ FRC2013_Main_Robot/src/frc2168_2013/CommandBaseRobot.java
@@ -8,7 +8,7 @@
 package frc2168_2013;
 
 
-import java.util.TimerTask;
+
 
 import com.sun.squawk.platform.posix.natives.Time;
 
@@ -33,8 +33,7 @@ import frc2168_2013.utils.SerialCommunicator;
 public class CommandBaseRobot extends IterativeRobot {
 
     Command autonomousCommand;
-    java.util.Timer executor;
-    double timeOld=0;
+
     
     Compressor compressor;
     
