108a1fc182a30ebd4367dd853ea21c10d63b0ede
Shriji <shriji.amin1995@gmail.com>
2013-02-27 12:48:19 -0500

    indexing done!

--- FRC2013_Main_Robot/src/frc2168_2013/commands/ShootSingleFrisbee.java
+++ FRC2013_Main_Robot/src/frc2168_2013/commands/ShootSingleFrisbee.java
@@ -15,7 +15,7 @@ public class ShootSingleFrisbee extends CommandGroup
 	
 		//TODO: Determine if this wait is necessary
 		//wait so there is a consistent entry velocity into the shooter wheel
-		//addSequential(new Sleep(), .200); //wait for 1/5 second;
+		addSequential(new Sleep(), .200); //wait for 1/5 second;
 	
 		//verify shooter is at speed
 		//addSequential(new PID_ShooterAtSpeed());
@@ -27,6 +27,6 @@ public class ShootSingleFrisbee extends CommandGroup
 		addSequential(new DriveUntilFrisbee(), 2);
 	
 		//shooter wheel will stop if timed out
-		//addSequential(new StopShooterWheel());
+		addSequential(new StopShooterWheel());
 	}
 }
